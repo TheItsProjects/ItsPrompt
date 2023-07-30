5e01359582f2e8fc631ffa0bcc8a9266614363ee
TheItsNameless <TheItsNameless@users.noreply.github.com>
2023-07-30 18:18:41 +0200

    Fix styling in style

--- ItsPrompt/data/style.py
+++ ItsPrompt/data/style.py
@@ -6,11 +6,11 @@ from prompt_toolkit.styles import Style
 
 @dataclass
 class PromptStyle:
-    '''
+    """
     The style object used for styling the prompts.
-    
+
     Empty styles will not be styled, so they appear without any styling.
-    '''
+    """
     question_mark: str = ''
     question: str = ''
     option: str = ''
@@ -19,26 +19,28 @@ class PromptStyle:
     error: str = ''
     text: str = ''
     grayout: str = ''
+    disabled: str = ''
 
 
 default_style = PromptStyle(
-    question_mark='fg:ansigreen',
-    selected_option='fg:ansicyan',
-    tooltip='fg:ansibrightblue bg:ansiwhite bold',
-    error='fg:ansiwhite bg:ansired bold',
-    grayout='fg:ansibrightblack',
+    question_mark="fg:ansigreen",
+    selected_option="fg:ansicyan",
+    tooltip="fg:ansibrightblue bg:ansiwhite bold",
+    error="fg:ansiwhite bg:ansired bold",
+    grayout="fg:ansibrightblack",
+    disabled="fg:ansibrightblack",
 )
 
 
 def convert_style(style: PromptStyle) -> Style:
-    '''
+    """
     Converts the given `PromptStyle` to a usable `Style` object.
 
     :param style: The style to convert
     :type style: PromptStyle
     :return: The converted `Style` object
     :rtype: Style
-    '''
+    """
     return Style.from_dict(asdict(style))
 
 
